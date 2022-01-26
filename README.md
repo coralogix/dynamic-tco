809db448cfe1e73725d5e8c067a18e6fa5e61566
Juan Gimenez <juan@coralogix.com>
2022-01-26 11:31:13 -0300

    # This is a combination of 13 commits. # This is the 1st commit message:
    Added end line
    
    # This is the commit message #2:
    
    Added TCO_ENDPOINT variable
    
    # This is the commit message #3:
    
    Cleanup
    
    # This is the commit message #4:
    
    Added Override object is empty validation
    
    # This is the commit message #5:
    
    removed unnecesary lines
    
    # This is the commit message #6:
    
    removed unnecesary lines
    
    # This is the commit message #7:
    
    Updated Readme with tco api documentation link
    
    # This is the commit message #8:
    
    updated resources in template
    
    # This is the commit message #9:
    
    Readme Update
    
    # This is the commit message #10:
    
    added variables details in readme
    
    # This is the commit message #11:
    
    added variables to readme
    
    # This is the commit message #12:
    
    Fixed Typo
    # This is the commit message #13:
    
    Added Cluster URL

--- README.md
+++ README.md
@@ -7,9 +7,8 @@ We do suggest that utilizing this tool would be a second line of defence to a Pa
 
 # Requirements:
 - An S3 bucket for the lambda to use
-- An IAM role for the lambda execution and the S3 (list, read, write)
 - Your aws User should have permissions to deploy lambdas.
-- Your User should have AWS CLI.
+- Your User should have AWS CLI and SAM.
 - Coralogix audit account access.
 
 # Installation of the solution
@@ -21,12 +20,39 @@ S3 is cheaper and robust.
 The lambda uses AWS SAM deployment framework.
 Below are the steps to install the lambda:
 - Clone this repository to the machine you wish to deploy from
-- Update the Values on the config file
 - Deploy the Lambda.
+```
+# sam build
+# sam deploy --guided
+```
+- Go to the created Lambda function and add AmazonS3FullAccess Policy to the Excecution Role.
+- Update Lambdas Environmental Variables from AWS Console.
 
+Variable |Description
+----------|------------
+APPLICATION_NAME |Application Name in Coralogix.
+AWS_BUCKET_NAME |Bucket the function will use to save tco status. Created in the requirements.
+FUNCTION_KEY |Secret key that is used to authenticate against the lambda function.
+PRIVATE_KEY |Coralogix Send your logs Api Key. Data Flow->ApiKeys
+SUBSYSTEM_NAME |Subsystem name in Coralogix
+TCO_KEY |Coralogix TCO Api Key. Data Flow->ApiKeys
+
+
+If your Account is not in the EU region (the account url does not have a .com suffix)
+you will need to add this environment variable:
+```
+CORALOGIX_LOG_URL=https://<coralogix_cluster_url>/api/v1/logs
+```
+Cluster location | coralogix_cluster_url
+-----------------| --------------------
+US| api.coralogix.us
+IN| api.app.coralogix.in
+Singapore| api.coralogixsg.com
 
 ## Setting the new TCO policies to enforce by the tool.
-
+This lambda uses Coralogix TCO API to enforce the new policies.
+In the Body of the request to the API Gateway will have an array of policies.
+To know more, please refere to https://coralogix.com/tutorials/tco-optimizer-api/ "Create new Policy" section.
 
 # Comments and Collaboration:
 If you have any Comment or Collaboration, please use github issue, create a pull request or just comment us in our Support Chat inside your Coralogix Account
--- tco_watchdog_lambda_app/tcowatchdog.py
+++ tco_watchdog_lambda_app/tcowatchdog.py
@@ -6,24 +6,25 @@ import requests
 import json
 import datetime
 
-
 class TcoWatchDog:
-    def __init__(self):
+    def __init__(self):   
         if not os.environ.get('PRIVATE_KEY'):
             raise Exception("Missing the PRIVATE_KEY environment variable. CANNOT CONTINUE")
         if not os.environ.get('TCO_KEY'):
-            raise Exception("Missing the TCO_KEY environment variable. CANNOT CONTINUE")
-    
+            raise Exception("Missing the TCO_KEY environment variable. CANNOT CONTINUE")   
+    #Take variables from environment   
     PRIVATE_KEY = os.environ.get('PRIVATE_KEY')
     APP_NAME = os.environ.get('APPLICATION_NAME', 'TCOWATCHDOG')
     SUB_SYSTEM = os.environ.get('SUBSYSTEM_NAME', 'TCOWATCHDOG')
     TCO_KEY = os.environ.get('TCO_KEY')
     AWS_BUCKET_NAME = os.environ.get('AWS_BUCKET_NAME')
-
+    coralogix_domain = os.environ.get('CORALOGIX_LOG_URL', 'https://api.coralogix.com/api/v1/logs').split("/")[2]
+    #Set up logger objects
     logger = logging.getLogger("Python Logger")
     logger.setLevel(logging.DEBUG)  
-    coralogix_handler = CoralogixLogger(PRIVATE_KEY, APP_NAME, SUB_SYSTEM)
-    logger.addHandler(coralogix_handler)
+    if not logger.handlers:
+        coralogix_handler = CoralogixLogger(PRIVATE_KEY, APP_NAME, SUB_SYSTEM)
+        logger.addHandler(coralogix_handler)
     s3_client = boto3.client('s3')
 
     def main (self, event, context, bucket_name = AWS_BUCKET_NAME):
@@ -32,29 +33,23 @@ class TcoWatchDog:
             "event":"Triggered"
             }
         self.logger.info(log)
-
+        #Get current status of TCO Policy and override
         listtco = TcoWatchDog.listTCO(self, event)
         listoverride = TcoWatchDog.listOverride(self, event)
+        #Save Current status of TCO and Override to S3 
         self.s3_client.put_object(Bucket=bucket_name,Key='listtco_latest.json', Body=listtco) 
         self.s3_client.put_object(Bucket=bucket_name,Key='listtco_'+datetime.datetime.now().isoformat()+'.json', Body=listtco)
         self.s3_client.put_object(Bucket=bucket_name,Key='listoverride_latest.json', Body=listoverride)
         self.s3_client.put_object(Bucket=bucket_name,Key='listoverride_'+datetime.datetime.now().isoformat()+'.json', Body=listoverride)
-        
-
-
         TcoWatchDog.delTCO(self, listtco)
-
-        TcoWatchDog.delOverride(self, listoverride)
-        
-        TcoWatchDog.applyTco(self, event, context)
-        
+        TcoWatchDog.delOverride(self, listoverride)        
+        TcoWatchDog.applyTco(self, event, context)        
         CoralogixLogger.flush_messages()
         
-
     def applyTco(self,event,context):
         new_rules=json.loads(event["body"])
         for element in new_rules:
-            arg = requests.post('https://api.coralogix.com/api/v1/external/tco/policies',
+            arg = requests.post('https://'+self.coralogix_domain+'/api/v1/external/tco/policies',
             headers = {'content-type': 'application/json', 'Authorization': "Bearer " + self.TCO_KEY}, json = element)
             log = {
                 "Event" : "Apply TCO",
@@ -67,16 +62,14 @@ class TcoWatchDog:
     def delOverride(self, str_override):
         overrides = json.loads(str_override)
         if len(overrides) == 0:
-            log = {}
             log = {
                 "Event" : " Deleting Override",
                 "log" : "No Override to Delete"
                 }
             self.logger.info(log)
             return None        
-        arg = requests.delete('https://api.coralogix.com/api/v1/external/tco/overrides/bulk',
-        headers = {'content-type': 'application/json', 'Authorization': "Bearer " + self.TCO_KEY}, json = overrides)
-            
+        arg = requests.delete('https://'+self.coralogix_domain+'/api/v1/external/tco/overrides/bulk',
+        headers = {'content-type': 'application/json', 'Authorization': "Bearer " + self.TCO_KEY}, json = overrides)            
         if arg.status_code != 200:
             print('Error Deleting Override')
             print(overrides)
@@ -84,8 +77,7 @@ class TcoWatchDog:
             "Event" : "Error Deleting Override",
             "log" : overrides
             }
-            self.logger.error(log)
-        
+            self.logger.error(log)        
         log = {
             "Event" : " Deleting Override",
             "log" : overrides
@@ -99,10 +91,9 @@ class TcoWatchDog:
                 "Event" : " Deleting Policy",
                 "log" : "No Policy to Delete"
                 }
-            self.logger.info(log)
-        
+            self.logger.info(log)        
         for element in tcos:
-            arg = requests.delete('https://api.coralogix.com/api/v1/external/tco/policies/'+element["id"],
+            arg = requests.delete('https://'+self.coralogix_domain+'/api/v1/external/tco/policies/'+element["id"],
                     headers = {'content-type': 'application/json', 'Authorization': "Bearer " + self.TCO_KEY}
                 )
             log = {
@@ -118,29 +109,28 @@ class TcoWatchDog:
                 }
                 self.logger.error(log)
             
-
     def listTCO(self, arg):
-        
-        arg = requests.get('https://api.coralogix.com/api/v1/external/tco/policies',
+        arg = requests.get('https://'+self.coralogix_domain+'/api/v1/external/tco/policies',
                     headers = { 'content-type': 'application/json', 'Authorization': "Bearer " + self.TCO_KEY}
                     )
+        new_content = json.loads(arg.content)
         log = {
             "Event" : "List Existing TCO",
-            "log" : arg.content
+            "log" : new_content
         }
         self.logger.info(log)
         return (arg.content)
     
     def listOverride(self, arg):
-        
-        arg = requests.get('https://api.coralogix.com/api/v1/external/tco/overrides',
+        arg = requests.get('https://'+self.coralogix_domain+'/api/v1/external/tco/overrides',
         headers = {
                         'content-type': 'application/json',
                         'Authorization': "Bearer " + self.TCO_KEY}
-        )
+        )        
+        new_content = json.loads(arg.content)
         log = {
             "Event":"List Existing TCO Overrides",
-            "log":arg.content
+            "log":new_content
         }
         self.logger.info(log)
         return (arg.content)

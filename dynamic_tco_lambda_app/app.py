de0e84d5c4bc5bc34c2159c5069d89927bced385
juan-coralogix <89215136+juan-coralogix@users.noreply.github.com>
2024-08-14 15:46:49 -0300

    Fixed lower case issue with headers
    Fixed lower case issue with headers

--- dynamic_tco_lambda_app/app.py
+++ dynamic_tco_lambda_app/app.py
@@ -8,8 +8,9 @@ def lambda_handler(event,context):
         tcoresetter = utcresetter.UtcResetter()
         tcoresetter.main(event, context)
     else:
-        obj = json.loads(json.dumps(event)) 
-        if obj["headers"]["Function-Key"] != os.environ.get('FUNCTION_KEY'):
+        obj = json.loads(json.dumps(event))
+        if obj["headers"]["function-key"] != os.environ.get('FUNCTION_KEY'):
+            
             print("Missing Auth Header !!")
             return {
             'statusCode': 401,

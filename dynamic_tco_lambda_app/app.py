9467cc039bcc29630ae2f96644f6f8e799594881
Noya Itzhaki <noya.itzhaki@coralogix.com>
2025-06-09 11:39:04 +0300

    Endpoint update

--- dynamic_tco_lambda_app/app.py
+++ dynamic_tco_lambda_app/app.py
@@ -9,8 +9,12 @@ def lambda_handler(event,context):
         tcoresetter.main(event, context)
     else:
         obj = json.loads(json.dumps(event))
-        if obj["headers"]["function-key"] != os.environ.get('FUNCTION_KEY'):
-            
+    # previous format (not working anymore)
+        # if obj["headers"]["function-key"] != os.environ.get('FUNCTION_KEY'):
+        #     print("headers: ", obj["headers"])
+    # new format (working)
+        headers = {k.lower(): v for k, v in obj.get("headers", {}).items()}
+        if headers.get("function-key") != os.environ.get('FUNCTION_KEY'):
             print("Missing Auth Header !!")
             return {
             'statusCode': 401,
